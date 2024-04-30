916b9a87cde0b8e8b7e62ea67c18f81408dcea3b
bjyoon <faizanemercy@gmail.com>
2024-04-30 10:28:43 +0900

    회원가입, 로그인, 라우터 추가

--- router/tweets.js
+++ router/tweets.js
@@ -1,7 +1,14 @@
 import express from "express";
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator';
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
+
+const validateTweet = [
+    body('text').trim().isLength({min:3}).withMessage('최소 3자 이상 입력'), validate
+]
+
 
 
 // 해당 아이디에 대한 트윗 가져오기
@@ -21,7 +28,7 @@ router.post('/', tweetController.getTweet);
 // name, username, text
 // json 형태로 입력 후 추가된 데이터까지 모두 json으로 출력
 
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 
 // 트윗 수정하기
@@ -30,7 +37,7 @@ router.post('/', tweetController.createTweet);
 // id, username, text
 // json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
 
-router.post('/', tweetController.updateTweet);
+router.post('/', validateTweet, tweetController.updateTweet);
 
 // 트윗 삭제하기
 // DELETE
